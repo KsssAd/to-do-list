529bf58216fa097aa5742fc41d6571f9b2ae8e4c
Ksenia Adarich <kseniaadaric@gmail.com>
2024-03-08 13:17:29 +0300

    Доработка для публикации проекта

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB5-TNb5YWX8YPIH-T-9_pjSaEAMyOwraA",
+    authDomain: "to-do-project-edd86.firebaseapp.com",
+    projectId: "to-do-project-edd86",
+    storageBucket: "to-do-project-edd86.appspot.com",
+    messagingSenderId: "720737302769",
+    appId: "1:720737302769:web:35df9eff459f3aeaff5150",
+    measurementId: "G-5W8XHTKQPM"
+  }
 };
